dfaa9d14760c69a83151fc829b908eac5458ef16
Pranat1 <74774569+Pranat1@users.noreply.github.com>
2021-03-31 15:53:48 +0530

    Add files via upload

---
+++ Scripts/Buildings/UnitsSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.EventSystems;
+
+public class UnitsSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject unitPrefab = null;
+    [SerializeField] private Transform unitSpownPoint = null;
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        GameObject unitInstance = Instantiate(
+            unitPrefab,
+            unitSpownPoint.position,
+            unitSpownPoint.rotation);
+
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+    }
+
+    #endregion
+    #region Client 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left) { return; }
+        if (!hasAuthority) { return; }
+        CmdSpawnUnit();
+    }
+    #endregion
+}
